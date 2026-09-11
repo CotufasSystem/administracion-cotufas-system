32675e6cb6ed097a336d9e07df85604aa5e5dd01
CotufasSystem <cotufassystem@users.noreply.github.com>
2026-09-11 11:28:59 -0400

    fix: importar useMemo en EmployeePortalDetailView

--- src/components/attendance/EmployeePortalDetailView.js
+++ src/components/attendance/EmployeePortalDetailView.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { THEME } from '../../constants/theme';
